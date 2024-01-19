c47448a99962399c2d6cc6a4cf63ff660737a551
jonathan <zlatanjon@hotmail.com>
2024-01-18 22:17:17 -0600

    :sparkles:(useRouter): Add useRouter hook and redirect to home page on error

--- src/app/api/providers/create/route.ts
+++ src/app/api/providers/create/route.ts
@@ -3,16 +3,20 @@ import { NextResponse, type NextRequest } from 'next/server'
 export async function POST(request: NextRequest) {
   const res = await request.json();
   try {
-    const response = await fetch(`${process.env.API_URL}/providers/create`, {
+    const response = await fetch(`${process.env.API_URL}providers/create`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(res)
     })
+    if(!response.ok) {
+      throw new Error(response.statusText || 'Unknown error')
+    }
     const data = await response.json()
     return NextResponse.json({ data }, { status: 200 });
   } catch (error) {
+    console.log("🚀 ~ POST ~ error:", error)
     return NextResponse.json({ error }, { status: 500 });
   }
 }
