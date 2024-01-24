810c4895753113bd7022c79a872515a6d2d1e196
jonathan <zlatanjon@hotmail.com>
2024-01-24 17:46:02 -0600

    :bug:(Providers):Fix router push and update select field rendering, filter data

--- src/app/(providers)/create/page.tsx
+++ src/app/(providers)/create/page.tsx
@@ -24,6 +24,8 @@ import DatePicker from "@/components/DatePicker"
 import { ToastAction } from "@/components/ui/toast"
 import IconPlus from "@/components/IconPlus"
 
+const IS_DEV = process.env.NODE_ENV !== "development"
+
 export type ContactValues = {
   title?: string,
   fullName?: string,
@@ -62,20 +64,20 @@ const Page = () => {
   const form = useForm(
     {
       defaultValues: {
-        businessName: process.env.NODE_ENV === "development" ? `Empresa ${(Math.random() * 1000).toFixed(0)}` : "",
-        commercialName: process.env.NODE_ENV === "development" ? "Empresa Prueba" : "",
-        website: process.env.NODE_ENV === "development" ? "www.example.com" : "",
-        constitutionDate: process.env.NODE_ENV === "development" ? new Date() : "",
+        businessName: IS_DEV ? `Empresa ${(Math.random() * 1000).toFixed(0)}` : "",
+        commercialName: IS_DEV ? "Empresa Prueba" : "",
+        website: IS_DEV ? "www.example.com" : "",
+        constitutionDate: IS_DEV ? new Date() : "",
         deletedDate: "",
         reactivatedDate: "",
-        state: process.env.NODE_ENV === "development" ? "Tabasco" : "",
-        fullAddress: process.env.NODE_ENV === "development" ? "Calle 1" : "",
-        postalCode: process.env.NODE_ENV === "development" ? "86000" : "",
-        delegation: process.env.NODE_ENV === "development" ? "Villahermosa" : "",
-        rfc: process.env.NODE_ENV === "development" ? `RFC${(Math.random() * 1000000).toFixed(0)}HT4` : "",
-        socialObjective: process.env.NODE_ENV === "development" ? "Hola mundo Objetivo Social" : "",
-        economicActivity: process.env.NODE_ENV === "development" ? "Hola mundo Actividad Economica" : "",
-        speciality: process.env.NODE_ENV === "development" ? "Hola mundo Especialidad" : "",
+        state: IS_DEV ? "Tabasco" : "",
+        fullAddress: IS_DEV ? "Calle 1" : "",
+        postalCode: IS_DEV ? "86000" : "",
+        delegation: IS_DEV ? "Villahermosa" : "",
+        rfc: IS_DEV ? `RFC${(Math.random() * 1000000).toFixed(0)}HT4` : "",
+        socialObjective: IS_DEV ? "Hola mundo Objetivo Social" : "",
+        economicActivity: IS_DEV ? "Hola mundo Actividad Economica" : "",
+        speciality: IS_DEV ? "Hola mundo Especialidad" : "",
         west: false,
         east: false,
         northeast: false,
@@ -84,11 +86,11 @@ const Page = () => {
         center: false,
         contact: [
           {
-            title: process.env.NODE_ENV === "development" ? "CEO" : "",
-            fullName: process.env.NODE_ENV === "development" ? "Juan Perez" : "",
-            officePhone: process.env.NODE_ENV === "development" ? "123456789" : "",
-            mobilePhone: process.env.NODE_ENV === "development" ? "123456789" : "",
-            email: process.env.NODE_ENV === "development" ? "radius@example.com" : "",
+            title: IS_DEV ? "CEO" : "",
+            fullName: IS_DEV ? "Juan Perez" : "",
+            officePhone: IS_DEV ? "123456789" : "",
+            mobilePhone: IS_DEV ? "123456789" : "",
+            email: IS_DEV ? "radius@example.com" : "",
           }
         ],
       }
@@ -96,19 +98,19 @@ const Page = () => {
   )
   const router = useRouter()
   const [loading, setLoading] = useState(false)
-  const { handleSubmit} = form
+  const { handleSubmit, reset } = form
   
   const onSubmit: SubmitHandler<FormValues> = async (data) => {
     setLoading(true)
     try {
-      await createProviders(data)
+      const response = await createProviders(data)
       toast({
-        title: "Success",
-        description: "Se creo el proveedor",
+        title: "Proveedor guardado con éxito",
+        description: `Se guardo al proveedor ${response?.data?.nomraz} con número ${response?.data?.id}.`,
         variant: "success",
         action: <ToastAction className="bg-radius text-white hover:bg-slate-500" altText="Ok">Ok</ToastAction>
       })
-      
+      reset()
     } catch (error) {
       toast({
         title: "Error",
@@ -136,33 +138,32 @@ const Page = () => {
               </CardHeader>
               <CardContent>
                 <div className="grid grid-flow-row md:grid-cols-4 gap-4">
-                  <InputField form={form} name="businessName" label="Razon Social" />
-                  <InputField form={form} name="commercialName" label="Nombre Comercial" />
+                  <InputField form={form} name="businessName" label="Razón social" />
+                  <InputField form={form} name="commercialName" label="Nombre comercial" />
                   <InputField form={form} name="rfc" label="RFC" />
-                  <InputField form={form} name="curp" label="CURP" />
                 </div>
                 <div className="grid grid-flow-row md:grid-cols-3 gap-4 py-2">
                   <div className="self-center">
-                    <DatePicker form={form} name="constitutionDate" label="Fecha de Constitucion" />
+                    <DatePicker form={form} name="constitutionDate" label="Fecha de constitución" />
                   </div>
                   <div className="col-span-2">
-                    <InputField form={form} name= "fullAddress" label="Domicilio Completo" />
+                    <InputField form={form} name= "fullAddress" label="Domicilio completo" />
                   </div>
                 </div>
                 <div className="grid grid-flow-row md:grid-cols-5 gap-4 py-2">
                   <InputField form={form} name= "state" label="Estado" />
-                  <InputField form={form} name= "delegation" label="Delegacion/Municipio" />
+                  <InputField form={form} name= "delegation" label="Delegación/Municipio" />
                   <InputField form={form} name= "postalCode" label="C.P." />
-                  <InputField form={form} name="website" label="Pagina Web" />
+                  <InputField form={form} name="website" label="Página web" />
                 </div>
                 <div className="grid grid-flow-3 md:grid-cols-5 gap-4 items-center ">
                   <div className="md:col-span-5 mt-6">
                     <Separator />
                   </div>
                   <div className=" md:col-start-2">
-                    <InputField form={form} name="socialObjective" label="Objetivo Social" multiple />
+                    <InputField form={form} name="socialObjective" label="Objetivo social" multiple />
                   </div>
-                    <InputField form={form} name="economicActivity" label="Actividad Economica" multiple />
+                    <InputField form={form} name="economicActivity" label="Actividad económica" multiple />
                     <InputField form={form} name="speciality" label="Especialidad" multiple />
                 </div>
               </CardContent>
@@ -186,9 +187,9 @@ const Page = () => {
                               return item?.title ? (
                               <div className="col-span-5 grid grid-cols-4 gap-4" key={`${item.title}_${index}`}>
                                 <InputField form={form} name={`contact.${index}.fullName`} label={item?.title || 'Titulo'} placeholder="Nombre completo"/>
-                                <InputField form={form} name={`contact.${index}.officePhone`} label="Telefono Oficina" />
-                                <InputField form={form} name={`contact.${index}.mobilePhone`} label="Telefono Movil" />
-                                <InputField form={form} name={`contact.${index}.email`} label="Correo Electronico" />
+                                <InputField form={form} name={`contact.${index}.officePhone`} label="Teléfono Oficina" />
+                                <InputField form={form} name={`contact.${index}.mobilePhone`} label="Teléfono Movil" />
+                                <InputField form={form} name={`contact.${index}.email`} label="Correo" />
                                 <div className="col-span-5 gap-4">
                                   <Separator />
                                 </div>
